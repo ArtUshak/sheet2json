79554fcdb7b2ea24fd5802a77533c25527221ee1
Artiom Khandamirov <t9max@yandex.ru>
2019-11-20 21:12:40 +0300

    Fix

--- run1.bat
+++ run1.bat
@@ -1,1 +1,1 @@
-poetry run python ./sheet2json/sheet2json.py --input-file "%1" --output-file "%~n1.json" & echo "Press any key to exit" & pause > nul
+poetry run python .\sheet2json\sheet2json.py --input-file "%1" --output-file "output\%~n1.json" & echo "Press any key to exit" & pause > nul
